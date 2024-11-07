58fd9739e08005c5ccec2c72a89ccb2ebe184a77
Asutosh Ghanto <asutosh@clevertap.com>
2024-11-07 16:25:49 +0530

    test

--- cmd/execute-runcommand/main.go
+++ cmd/execute-runcommand/main.go
@@ -1,189 +1,103 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"math/rand"
-	"net/http"
 	"os"
-	"strings"
-	"sync"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
-	"gopkg.in/matryer/try.v1"
+	"github.com/sensu/sensu-go/types"
 )
 
-var instance_id []*string
-var ssmOutput *ssm.Command
-
-func fetchMetaData(value string) string {
-	client := &http.Client{}
-
-	req, err := http.NewRequest("PUT", "http://169.254.169.254/latest/api/token", nil)
-	if err != nil {
-		log.Fatalf("Error creating request: %v", err)
-	}
-	req.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", "3600")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Error making PUT request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	token, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading token response body: %v", err)
-	}
-
-	url := "http://169.254.169.254/latest/meta-data/" + value + "/"
-	req, err = http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatalf("Error creating GET request: %v", err)
-	}
-	// Pass the token in the header
-	req.Header.Set("X-aws-ec2-metadata-token", strings.TrimSpace(string(token)))
-
-	// Step 3: Execute the request
-	resp, err = client.Do(req)
-	if err != nil {
-		log.Fatalf("Error making GET request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Read response body
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error while reading metadata response body: %v", err)
-	}
-	return string(body)
-}
-
-var (
-	myRetryer = client.DefaultRetryer{
-		NumMaxRetries:    10,
-		MaxThrottleDelay: 2 * time.Second,
-	}
-	sess = session.Must(session.NewSessionWithOptions(
-		session.Options{
-			Config: aws.Config{
-				Retryer: myRetryer,
-				Region:  aws.String(fetchMetaData("placement/region")),
+func getInstanceIDByIP(ec2Client *ec2.EC2, ipAddress string) (string, error) {
+	input := &ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			{
+				Name:   aws.String("private-ip-address"),
+				Values: []*string{aws.String(ipAddress)},
 			},
-			SharedConfigState: session.SharedConfigEnable,
-		}))
-	ssmSession = ssm.New(sess)
-	command    = "export container_id_nginx=$(sudo docker ps | grep testrepo | awk  {'print $1'}) && echo ${container_id_nginx} && sudo docker stop ${container_id_nginx}"
-)
-
-// $hkgfe
-func main() {
-
-	input := ssm.SendCommandInput{
-		TimeoutSeconds: aws.Int64(300),
-		InstanceIds:    instance_id,
-		DocumentName:   aws.String("AWS-RunShellScript"),
-		Comment:        aws.String("Remidiation action"),
-	}
-	input.Parameters = map[string][]*string{
-		"commands":         aws.StringSlice([]string{command}),
-		"executionTimeout": aws.StringSlice([]string{fmt.Sprintf("%d", 3600)}),
+		},
 	}
 
-	var output *ssm.SendCommandOutput
-	rand.Seed(time.Now().UnixNano())
-	time.Sleep(time.Duration(rand.Intn(3600)) * time.Second)
-	err := try.Do(func(attempt int) (bool, error) {
-		var err error
-		output, err = ssmSession.SendCommand(&input)
-		ssmOutput = output.Command
-		if err != nil {
-			time.Sleep(time.Duration(rand.Intn(7200)) * time.Second)
-		}
-		return attempt < 2, err
-	})
+	result, err := ec2Client.DescribeInstances(input)
 	if err != nil {
-		fmt.Printf("Error Invoking SSM Run Command: %s", err)
-		os.Exit(3)
+		return "", fmt.Errorf("failed to describe instances: %w", err)
 	}
 
-	var wg sync.WaitGroup
-	wg.Wait()
-	err = ssmSession.WaitUntilCommandExecuted(&ssm.GetCommandInvocationInput{
-		CommandId:  output.Command.CommandId,
-		InstanceId: instance_id[0],
-	})
-
-	if err != nil {
-		fmt.Printf("Error Executing command on Target Instance %s. Failed with Error %s", *instance_id[0], err)
-		os.Exit(2)
-	}
-
-	if ssmOutput == nil {
-		fmt.Println("Command not yet Executed")
-		os.Exit(1)
-	} else {
-		if (*(ssmOutput).Status) == "Pending" {
-			resp, err := ssmSession.GetCommandInvocation(&ssm.GetCommandInvocationInput{
-				CommandId:  output.Command.CommandId,
-				InstanceId: instance_id[0],
-			})
-			if err != nil {
-				fmt.Printf("Error Fetching Command Output after Executing %s", err)
-				os.Exit(2)
-			} else {
-				diskCheckOutput := aws.StringValue(resp.StandardOutputContent)
-
-				fmt.Println(diskCheckOutput)
-				os.Exit(0)
-			}
+	for _, reservation := range result.Reservations {
+		for _, instance := range reservation.Instances {
+			return *instance.InstanceId, nil
 		}
 	}
+	return "", fmt.Errorf("instance with IP address %s not found", ipAddress)
 }
 
-func init() {
+// SensuCheckAnnotation keys
+const (
+	regionAnnotation  = "region"
+	ipAddressAnnotation = "ip_address"
+	containerName      = "testrepo" // Name of the container to restart
+)
 
-	client := &http.Client{}
-	req, err := http.NewRequest("PUT", "http://169.254.169.254/latest/api/token", nil)
-	if err != nil {
-		log.Fatalf("Error creating request: %v", err)
+// Handler function for parsing annotations and firing SSM command
+func handler(event types.Event) error {
+	// Extract annotations
+	region, ok := event.Check.Annotations[regionAnnotation]
+	if !ok {
+		return fmt.Errorf("missing %s annotation in check", regionAnnotation)
 	}
-	req.Header.Set("X-aws-ec2-metadata-token-ttl-seconds", "3600")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Error making PUT request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	token, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("Error reading token response body: %v", err)
+	ipAddress, ok := event.Check.Annotations[ipAddressAnnotation]
+	if !ok {
+		return fmt.Errorf("missing %s annotation in check", ipAddressAnnotation)
 	}
 
-	url := "http://169.254.169.254/latest/meta-data/instance-id/"
-	req, err = http.NewRequest("GET", url, nil)
+	// Initialize AWS session
+	sess, err := session.NewSession(&aws.Config{Region: aws.String(region)})
 	if err != nil {
-		log.Fatalf("Error creating GET request for instanceID: %v", err)
+		return fmt.Errorf("failed to create AWS session: %w", err)
 	}
-	// Pass the token in the header
-	req.Header.Set("X-aws-ec2-metadata-token", strings.TrimSpace(string(token)))
 
-	// Step 3: Execute the request
-	resp, err = client.Do(req)
+	ec2Client := ec2.New(sess)
+	instanceID, err := getInstanceIDByIP(ec2Client, ipAddress)
 	if err != nil {
-		log.Fatalf("Error making GET request while getting instanceID: %v", err)
+		return fmt.Errorf("failed to get instance ID for IP %s: %w", ipAddress, err)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+
+	ssmClient := ssm.New(sess)
+
+	// Define SSM command to restart container
+	command := fmt.Sprintf("export container_id_nginx=$(sudo docker ps | grep %s | awk  {'print $1'}) && echo ${container_id_nginx} && sudo docker stop ${container_id_nginx}", containerName)
+	input := &ssm.SendCommandInput{
+		DocumentName: aws.String("AWS-RunShellScript"),
+		Parameters: map[string][]*string{
+			"commands": {aws.String(command)},
+		},
+		InstanceIds: []*string{aws.String(instanceID)},
+	}
+
+	// Execute SSM command
+	result, err := ssmClient.SendCommand(input)
 	if err != nil {
-		log.Fatalf("Error while reading metadata response body: %v", err)
+		return fmt.Errorf("failed to execute SSM command: %w", err)
 	}
-	id := string(body[:])
-	instance_id = append(instance_id, &id)
+
+	log.Printf("SSM command sent successfully: %s", *result.Command.CommandId)
+	return nil
+}
+
+func main() {
+	// Read Sensu event data from stdin
+	var event types.Event
+	if err := json.NewDecoder(os.Stdin).Decode(&event); err != nil {
+		log.Fatalf("Error decoding Sensu event: %v", err)
+	}
+
+	// Run the handler
+	if err := handler(event); err != nil {
+		log.Fatalf("Handler error: %v", err)
+	}
 }
